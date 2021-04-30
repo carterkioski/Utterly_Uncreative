d05d0f30d0e40571a28345423e5110247a4e1f18
ECHO <edward.ober@gmail.com>
2021-04-29 20:45:01 -0400

    added top 15 table

---
+++ Data/SQL/103-load_wine_count.sql
@@ -0,0 +1,2 @@
+COPY wine_count(variety,variety_count, winery_count,vintage_low,vintage_high)
+FROM 'C:\Users\edwar\Desktop\Projects\Utterly_Uncreative\Data\csv\wine_count.csv' DELIMITER ',' CSV HEADER;
